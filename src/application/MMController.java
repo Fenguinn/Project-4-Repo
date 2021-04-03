b820021aa05783dd7792b9f321ea21aa1ff28bd6
gent <gentblaku@gmail.com>
2021-04-02 23:52:42 -0400

    Main menu starts

---
+++ src/application/MMController.java
@@ -0,0 +1,41 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;  
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import application.orderDonutController;
+
+public class MMController {
+	
+	
+	//public MMController() {
+		
+	//}
+
+	@FXML
+	private ImageView mmDonutImage;
+	
+	@FXML
+	private static void openDonutPage(ActionEvent onMouseClicked) {
+		try {
+		    FXMLLoader fxmlLoader = new FXMLLoader(orderDonutController.class.getResource("Ordering Donuts.fxml"));
+		    Parent root1 = (Parent) fxmlLoader.load();
+		    Stage stage = new Stage();
+		    stage.initModality(Modality.APPLICATION_MODAL);
+		    stage.initStyle(StageStyle.UNDECORATED);
+		    stage.setTitle("Donut Menu");
+		    stage.setScene(new Scene(root1));  
+		    stage.show();
+		}
+		catch (Exception e) {
+			System.out.println("EXCEPTION GENT");
+		}
+	}
+	
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,16 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.control.ComboBox;
+import application.MMController;
+import application.orderDonutController;
+import application.currentOrderController;
+import application.storeOrderController;
+
 
 
 public class Main extends Application {
@@ -25,9 +31,10 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main Menu.fxml"));
+			Scene scene = new Scene(root,1000,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Main Menu");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			
