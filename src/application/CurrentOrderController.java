3bc2de92cd85a9ff9a46d8c8c4e4bec10c9d80bb
Fenguinn <salem.abdulla15@gmail.com>
2021-04-03 00:26:43 -0400

    AnchorPane removed for Coffee

---
+++ src/application/CurrentOrderController.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.control.TextArea;
+
+public class CurrentOrderController {
+	@FXML
+	private TextArea TextArea;
+	@FXML
+	private TextField SubTotalBox;
+	@FXML
+	private TextField TaxBox;
+	@FXML
+	private TextField TotalBox;
+	@FXML
+	private Button RemoveSelectedButton;
+	@FXML
+	private Button PlaceOrderButton;
+
+}
