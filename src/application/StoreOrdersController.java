4fdd6280982b77d38602ff45e1f96d728ad5f4eb
Fenguinn <salem.abdulla15@gmail.com>
2021-04-03 15:51:58 -0400

    Worked on Store Order GUI

---
+++ src/application/StoreOrdersController.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.control.TextArea;
+
+import javafx.scene.control.ComboBox;
+
+public class StoreOrdersController {
+	@FXML
+	private TextArea TextArea;
+	@FXML
+	private Button CancelOrderButton;
+	@FXML
+	private Button ExportButton;
+	@FXML
+	private TextField TotalBox;
+	@FXML
+	private ComboBox<Integer> OrderNumberBoxs;
+	
+}
